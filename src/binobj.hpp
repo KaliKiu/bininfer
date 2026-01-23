9484a2b5c664215f297f4e77bb8ac8c64662abf5
KaliKiu <palablink@gmail.com>
2026-01-23 11:26:14 +0800

    constructor binobj finished

--- src/binobj.hpp
+++ src/binobj.hpp
@@ -10,6 +10,7 @@ class Binobj{
     private:
         std::string FILE_PATH;
         std::vector<uint8_t> file;
+        std::size_t file_size;
 
     public:
         Binobj(std::string FILE_PATH_): FILE_PATH(FILE_PATH_){
@@ -17,7 +18,14 @@ class Binobj{
             if(!binfile.good()){
                 std::cout<<"Binobj Constructor: File input failed"<<std::endl;
             }
-            
+            std::streamsize file_size_ = binfile.tellg();
+            file_size=file_size_;
+
+            binfile.seekg(0,std::ios::beg);
+            std::vector<uint8_t> file_(file_size_);
+            if(!binfile.read(reinterpret_cast<char*>(file_.data()),file_size_)){
+                throw std::runtime_error("Failed to read");
+            }
         }
 
 };
